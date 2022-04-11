aff5e99d5d10dcfe68fbf5fd8c44f6340fb08cce
Holly <hcli@thoughtworks.com>
2022-04-11 09:48:30 +0800

    fix: selector is not reactive for async tasks

---
+++ Tests/RecoilReactiveTests.swift
@@ -0,0 +1,66 @@
+import SwiftUI
+import XCTest
+import Combine
+
+@testable import RecoilSwift
+
+final class RecoilReactiveTests: XCTestCase {
+  typealias Selector = RecoilSwift.Selector
+  
+  struct TestModule  {
+    static var stringAtom: Atom<String>!
+    static var upstreamSyncState: Selector<String>!
+    static var downstreamSyncState: Selector<String>!
+    
+    static var upstreamAsyncState: AsyncSelector<String, Error>!
+    static var downstreamAsyncState: AsyncSelector<String, Error>!
+  }
+  
+  override func setUp() {
+    TestModule.stringAtom = atom { "rawValue" }
+    TestModule.upstreamSyncState = selector { _ throws -> String in "sync value" }
+    TestModule.downstreamSyncState = selector { get throws -> String in
+      let string = get(TestModule.upstreamSyncState)
+      return string.uppercased()
+    }
+    TestModule.upstreamAsyncState = selector { get -> AnyPublisher<String, Error> in
+      Deferred {
+        Future { promise in
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            promise(Result.success("async value"))
+          }
+        }
+      }.eraseToAnyPublisher()
+    }
+    TestModule.downstreamAsyncState = selector { get throws -> String in
+      let string = get(TestModule.upstreamAsyncState) ?? ""
+      return string.uppercased()
+    }
+  }
+  
+  func testShouldGetValueFromUpstreamSyncSelector() {
+    let tester = HookTester {
+      useRecoilValue(TestModule.downstreamSyncState)
+    }
+    
+    XCTAssertEqual(tester.value, "sync value".uppercased())
+  }
+  
+  func testShouldGetValueFromUpstreamAsyncSelector() {
+    let expectation = XCTestExpectation(description: "Async value reovled")
+    
+    let tester = HookTester { () -> LoadBox<String, Error> in
+      let loadable = useRecoilValueLoadable(TestModule.downstreamAsyncState)
+      
+      if let value = loadable.data, value == "async value".uppercased() {
+        expectation.fulfill()
+      }
+      
+      return loadable
+    }
+    
+    XCTAssertNil(tester.value.data)
+
+    wait(for: [expectation], timeout: 0.3)
+  }
+}
