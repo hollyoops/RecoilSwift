5c108bb3df80f921100222c62a65f3556cdb320f
Holly <hcli@thoughtworks.com>
2023-01-07 23:26:32 +0800

    refactor: simplfy async timeout extension

--- Sources/Common/Extensions/Async+Operators.swift
+++ Sources/Common/Extensions/Async+Operators.swift
@@ -1,67 +1,30 @@
-/// Adds timeout that races with `run`.
+import Foundation.NSDate // for TimeInterval
+
+struct TimedoutError: Error, Equatable {}
+
 ///
-public func withTimeout<B>(
-    seconds: UInt,
-    run: @escaping () async throws -> B
-) -> () async throws -> B {
-    {
-        try await withTimeout(nanoseconds: UInt64(seconds) * 1_000_000_000) { () in
-            try await run()
-        }()
-    }
-}
-
-public func withTimeout<B>(
-    nanoseconds: UInt64,
-    run: @escaping () async throws -> B
-) -> () async throws -> B {
-    {
-        try await withTimeout(nanoseconds: nanoseconds, run: { () in
-            try await run()
-        })(())
-    }
-}
-
-public func withTimeout<A, B>(
-    nanoseconds: UInt64,
-    run: @escaping (A) async throws -> B
-) -> (A) async throws -> B {
-    { a in
-        let timeout: (A) async throws -> B? = { _ in
-            try await Task.sleep(nanoseconds: nanoseconds)
-            return .none
+/// Execute an operation in the current task subject to a timeout.
+///
+/// - Parameters:
+///   - seconds: The duration in seconds `operation` is allowed to run before timing out.
+///   - operation: The async operation to perform.
+/// - Returns: Returns the result of `operation` if it completed in time.
+/// - Throws: Throws ``TimedOutError`` if the timeout expires before `operation` completes.
+///   If `operation` throws an error before the timeout expires, that error is propagated to the caller.
+func withTimeout<T: Sendable>(
+    seconds: TimeInterval,
+    body: @escaping @Sendable () async throws -> T
+) async throws -> T {
+    try await withThrowingTaskGroup(of: T.self) { group -> T in
+        group.addTask {
+            return try await body()
         }
-
-        let race = asyncFirst([run, timeout])
-
-        if let value = try await race(a) {
-            return value
+        group.addTask {
+            try await Task.sleep(nanoseconds: UInt64(seconds * 1_000_000_000))
+            throw TimedoutError()
         }
-        else {
-            throw TimeoutCancellationError()
-        }
-    }
-}
-
-// MARK: - TimeoutCancellationError
-public struct TimeoutCancellationError : Error {}
-
-/// Runs multiple `fs` concurrently and returns the first arrival, which can be either success or error.
-/// This method is equivalent to `Promise.race` in JavaScript.
-public func asyncFirst<A, B>(_ fs: [(A) async throws -> B]) -> (A) async throws -> B {
-    precondition(!fs.isEmpty, "asyncFirst error: async array is empty.")
-
-    return { a in
-        try await withThrowingTaskGroup(of: B.self) { group -> B in
-            for f in fs {
-                group.addTask {
-                    try await f(a)
-                }
-            }
-
-            let first = try await group.next()!
-            group.cancelAll()
-            return first
-        }
+        let success = try await group.next()!
+        group.cancelAll()
+        return success
     }
 }
