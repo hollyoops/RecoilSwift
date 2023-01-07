13c1f0aa774deb23e32494445650f04e715d85c1
Holly <hcli@thoughtworks.com>
2023-01-07 23:23:17 +0800

    fix: combine async bridge can not be cancel issue

--- Sources/Common/Extensions/Combine+Async.swift
+++ Sources/Common/Extensions/Combine+Async.swift
@@ -1,6 +1,5 @@
 #if canImport(Combine)
 import Combine
-#endif
 
 public enum AnyPublisherError: Error {
     case finishedWithoutValue
@@ -8,24 +7,60 @@ public enum AnyPublisherError: Error {
 
 extension AnyPublisher {
     func async() async throws -> Output {
-        try await withCheckedThrowingContinuation { continuation in
-            var cancellable: AnyCancellable?
-            var finishedWithoutValue = true
-            cancellable = first()
-                .sink { result in
-                    switch result {
-                    case .finished:
-                        if finishedWithoutValue {
-                            continuation.resume(throwing: AnyPublisherError.finishedWithoutValue)
-                        }
-                    case let .failure(error):
-                        continuation.resume(throwing: error)
-                    }
-                    cancellable?.cancel()
-                } receiveValue: { value in
-                    finishedWithoutValue = false
-                    continuation.resume(with: .success(value))
+        let cancellableActor = CancellableActor()
+        return try await withTaskCancellationHandler(operation: {
+            try Task.checkCancellation()
+            
+            return try await withCheckedThrowingContinuation { continuation in
+                guard !Task.isCancelled else {
+                    continuation.resume(throwing: CancellationError())
+                    return
                 }
-        }
+                
+                var didSendValue = false
+                let cancellable = self.first()
+                    .handleEvents(receiveCancel: {
+                        continuation.resume(throwing: CancellationError())
+                    })
+                    .sink(
+                        receiveCompletion: { completion in
+                            switch completion {
+                            case .finished:
+                                if !didSendValue {
+                                    continuation.resume(throwing: AnyPublisherError.finishedWithoutValue)
+                                }
+                                break
+                            case let .failure(error):
+                                continuation.resume(throwing: error)
+                            }
+                        },
+                        receiveValue: { value in
+                            didSendValue = true
+                            continuation.resume(returning: value)
+                        }
+                    )
+                Task {
+                    await cancellableActor.setCancellable(cancellable)
+                }
+            }
+        }, onCancel: {
+            Task {
+                await cancellableActor.cancel()
+            }
+        })
+    }
+}
+
+#endif
+
+actor CancellableActor {
+    private var cancellable: AnyCancellable?
+    
+    func setCancellable(_ cancellable: AnyCancellable) {
+        self.cancellable = cancellable
+    }
+    
+    func cancel() {
+        cancellable?.cancel()
     }
 }
